724820182282fe284fe3601aa103fb25eb63a217
DT <tempust85@hotmail.com>
2018-03-19 12:55:18 +1000

    Everything works now, no crashes.
    - JKA menu mouse works again as well

--- code/rd-gl2/imgui_docks_openjk/dock_shadereditor.cpp
+++ code/rd-gl2/imgui_docks_openjk/dock_shadereditor.cpp
@@ -16,7 +16,7 @@ const char *DockShaders::label() {
 }
 
 extern int shaders_next_id;
-extern shaderProgram_t *shaders[256];
+extern shaderProgram_t *shaders[2048];
 
 bool IsKeyPressedMap(ImGuiKey key, bool repeat = true);
 
@@ -79,9 +79,11 @@ void DockShaders::recompileShader() {
 //}
 
 void DockShaders::imgui() {
-
-
-	#define NUM_SHADERS 512
+	if (shaders_next_id == 0) {
+		ImGui::Text("no shaders registered");
+		return;
+	}
+	#define NUM_SHADERS 2048
 	int num_shaders = shaders_next_id;
 	//shaderProgram_t *shaders[NUM_SHADERS];
 	char items[NUM_SHADERS][256];
